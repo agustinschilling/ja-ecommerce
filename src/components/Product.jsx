1c4f9c8b345aa7ae0acc0be3d445be013a3c481a
Agustin <schillingagustin@gmail.com>
2021-07-27 21:32:48 -0300

    product card initial version

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-function Product() {
+const Product = (props) => {
     return (
-        <div>
-            
+        <div className="card mt-4 ms-4" style={{width: 300 }}>
+            <img src={props.img} class="card-img-top" alt="...">
+            </img>
+            <div class="card-body">
+                <h5 className="card-title">{props.name}</h5>
+                <p className="card-text">{props.desc}</p>
+                <a href="#" className="btn btn-success">Add to cart</a>
+            </div>
         </div>
     )
 }
